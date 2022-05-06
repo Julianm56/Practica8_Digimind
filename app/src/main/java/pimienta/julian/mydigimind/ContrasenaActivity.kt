8af895b57ee32e164a7c16d7f5c3480b64f9ba44
Julian Javier Pimeinta Rendon <jpimientarm56@gmail.com>
2022-05-05 17:25:57 -0700

    Added new activities: activity_contrasena, activity_contrasena and activity_registro.xml

---
+++ app/src/main/java/pimienta/julian/mydigimind/ContrasenaActivity.kt
@@ -0,0 +1,12 @@
+package pimienta.julian.mydigimind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pimienta.julian.mydigimind.R
+
+class ContrasenaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+    }
+}
